81a80323ce256936740c5b2546e7361818ce5a28
Linyong Shi <linyong.shi@student.manchester.ac.uk>
2016-11-24 19:47:19 +0000

    first last name bugs fix

--- src/comp62521/database/database.py
+++ src/comp62521/database/database.py
@@ -270,7 +270,7 @@ class Database:
         header=collection[0]
         data=collection[1]
 
-        # data=[data[i] for i in range(len(data)) if self.authors[i].name==searchText]
+        ##data=[data[i] for i in range(len(data)) if self.authors[i].name==searchText]
         data=[data[i] for i in range(len(data)) if searchText in self.authors[i].name]
 
         return (header, data)
